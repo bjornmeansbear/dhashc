03438e85bf953fce1cbe24ccb7e75431482c36e3
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-07-16 15:57:14 -0400

    trying to center sections… running into some JS issues
    the heights aren't set until much later… maybe I can somehow get it to
    measure the height of internal children?

--- js/dc.js
+++ js/dc.js
@@ -1,3 +1,18 @@
+$(document).ready(function() {
+  var wh = $(window).height();
+  $(".subsection").height(wh);
+  $(".subsection section.container").each(function() {
+    var sh = $(this).find(".row").height();
+    $(this).css("padding-top",(wh-sh)/2);
+  });
+});
+
+$(".scroll").click(function(event){    
+  event.preventDefault();
+  $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+});
+
+
 /*
 $(document).ready(function() {
   $(".slide").hide();
