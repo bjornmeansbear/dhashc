9547af628c7609268c71fad4c893984e0379da54
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-07-17 17:51:11 -0400

    ugh.

--- js/dc.js
+++ js/dc.js
@@ -1,16 +1,35 @@
-/*
-$(document).ready(function() {
-  $(".slide").hide();
-  $(".fadein.animate").hide();
+$(".fadeorder").hide();
 
-  var fs = 3000;
-  
-  $(".slide.first").fadeIn(fs, function () {
-    $(".fadein.one").fadeIn(fs, function () {
-      $(".fadein.two").fadeIn(fs, function () {
-        $(".fadein.three").fadeIn(fs);
-      });
-    });
-  });
+var uno = $('.columnss .cycle-slideshow.uno');
+
+uno.on('cycle-finished', function(event, opts) {
+  hide();
 });
-*/
+
+
+var outer = $('#slideshow');
+var inners = $('.inner-slideshow');
+var output = $('#output');
+
+inners.on( 'cycle-finished', function( e, opts, curr, next ) {
+    setTimeout(function() {
+        outer.cycle( 'next' );
+    }, 3000);
+});
+
+$('#slideshow').on( 'cycle-before', function( e, opts, curr, next ) {
+    // ignore bubbled events from inner slideshows
+    if (e.target !== this)
+        return;
+
+    inners.cycle('stop');
+
+    var index = opts.slides.index( next );
+    output.html( 'starting slideshow #' + (index+1) );
+
+    // start the next slideshow
+    $( next ).find('.inner-slideshow').cycle('destroy').cycle({
+        timeout: 3000
+    });
+});
+
