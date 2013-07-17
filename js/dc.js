74d0aafbc03387a4ca8cebedc73454f5ef1a8e49
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-07-17 16:14:26 -0400

    delete nav and goofy screen sizing

--- js/dc.js
+++ js/dc.js
@@ -1,16 +1,3 @@
-$(document).ready(function() {
-  var wh = $(window).height();
-  $(".subsection").height(wh);
-  $(".subsection section.container").each(function() {
-    var sh = $(this).find(".row").height();
-    $(this).css("padding-top",(wh-sh)/2);
-  });
-});
-
-$(".scroll").click(function(event){    
-  event.preventDefault();
-  $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
-});
 
 
 /*
